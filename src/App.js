4b0e8a3fdfc7ca3f5aaa9cf57141bba13c5f1b11
papav-hub <pavi03@kpu.ac.kr>
2021-01-16 06:42:45 +0900

    escape

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Customer from './components/Customer'
 import './App.css';
+
+const c = {
+  'name' : '최혜민',
+  'birthday' : '000323',
+  'gender' : '여자',
+  'job' : '백수'
+}
+// customer 라는 변수를 만듦
 
 class App extends Component {
   render() {
     return(
-      <div className = "gray-background">
-        <img src = { logo } lat = "logo" />
-        <h2> Let's develop management system! </h2>
-      </div>
+      <Customer
+        name = {c.name}
+        birthday = {c.birthday}
+        gender = {c.gender}
+        job = {c.job}
+      />
     );
   }
 }
+
+// Component -> 웹 문서에서 보여주기 위한 단위
 
 export default App;
---
+++ src/components/Customer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+// import -> 특정 라이브러리 불러오기
+// react component 라이브러리를 사용하기 위해서 import
+// 다른 것에서 이거 사용하기 위해서 export 
+
+class Customer extends React.Component {
+    render(){
+        return (
+            <div>
+                <h2>{this.props.name}</h2>
+                <p>{this.props.birthday}</p>
+                <p>{this.props.gender}</p>
+                <p>{this.props.job}</p>
+            </div>
+        )
+    }
+}
+// customer 클래스는 react의 component 형태로 작성 된 클래스
+// App.js로 간다 슝
+
+// data 출력할때는 프롭스 이용
+
+export default Customer;
+// 내보내기
