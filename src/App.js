64e0fd6557d206981c0a891cee9272f014dd2ac5
papav-hub <pavi03@kpu.ac.kr>
2021-01-17 04:59:46 +0900

    구조화

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import Customer from './components/Customer'
 import './App.css';
 
 const c = {
+  'id' : '1',
+  'image' : 'http://placeimg.com/500/500/any',
   'name' : '최혜민',
   'birthday' : '000323',
   'gender' : '여자',
-  'job' : '백수'
+  'job' : '백수이고 싶지만 취직하고 싶은 백수'
 }
 // customer 라는 변수를 만듦
+// 위의 링크는 랜덤으로 image 보여주는 사이트 / 64/64/는 이미지 사이즈
 
 class App extends Component {
   render() {
     return(
       <Customer
+        id = {c.id}
+        image = {c.image}
         name = {c.name}
         birthday = {c.birthday}
         gender = {c.gender}
@@ -22,6 +27,7 @@ class App extends Component {
     );
   }
 }
+// 경로
 
 // Component -> 웹 문서에서 보여주기 위한 단위
 
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -7,16 +7,38 @@ class Customer extends React.Component {
     render(){
         return (
             <div>
-                <h2>{this.props.name}</h2>
-                <p>{this.props.birthday}</p>
-                <p>{this.props.gender}</p>
-                <p>{this.props.job}</p>
+                <CustomerProfile id={this.props.id} image={this.props.image} name={this.props.name} />
+                <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job} />
             </div>
         )
     }
 }
 // customer 클래스는 react의 component 형태로 작성 된 클래스
 // App.js로 간다 슝
+
+
+class CustomerProfile extends React.Component{
+    render(){
+        return (
+            <div>
+                <img src = {this.props.image} art = "profile"/>
+                <h2> {this.props.name} ({this.props.id}) </h2>
+            </div>
+        )
+    }
+}
+
+class CustomerInfo extends React.Component {
+    render(){
+        return (
+            <div>
+                <p> {this.props.birthday} </p>
+                <p>{this.props.gender}</p>
+                <p>{this.props.job}</p>
+            </div>
+        )
+    }
+}
 
 
 
