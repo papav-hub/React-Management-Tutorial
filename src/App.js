ba5a583e9ce6f07ace6d49fbfa25c28480b9fb25
papav-hub <pavi03@kpu.ac.kr>
2021-01-17 05:10:12 +0900

    map함수

--- src/App.js
+++ src/App.js
@@ -2,28 +2,56 @@ import React, { Component } from 'react';
 import Customer from './components/Customer'
 import './App.css';
 
-const c = {
-  'id' : '1',
-  'image' : 'http://placeimg.com/500/500/any',
+const c = [
+  {
+  'id' : 1,
+  'image' : 'http://placeimg.com/300/300/1',
   'name' : '최혜민',
   'birthday' : '000323',
   'gender' : '여자',
   'job' : '백수이고 싶지만 취직하고 싶은 백수'
+},
+{
+  'id' : 2,
+  'image' : 'http://placeimg.com/300/300/2',
+  'name' : '최형우',
+  'birthday' : '070607',
+  'gender' : '남자',
+  'job' : '한 손에 흑염룡이 잠든 중2'
+},
+{
+  'id' : 3,
+  'image' : 'http://placeimg.com/300/300/3',
+  'name' : '최준혁',
+  'birthday' : '000305',
+  'gender' : '남자',
+  'job' : '곧 군대가는 멍청이'
 }
+]
 // customer 라는 변수를 만듦
 // 위의 링크는 랜덤으로 image 보여주는 사이트 / 64/64/는 이미지 사이즈
 
 class App extends Component {
   render() {
     return(
-      <Customer
-        id = {c.id}
-        image = {c.image}
-        name = {c.name}
-        birthday = {c.birthday}
-        gender = {c.gender}
-        job = {c.job}
-      />
+      <div>
+        {
+          c.map(cs => {
+            return (
+              <Customer
+                key = {cs.id}
+                // id값을 key로 사용하겠다. (map 함수 사용할때 사용)
+                id = {cs.id}
+                image = {cs.image}
+                name = {cs.name}
+                birthday = {cs.birthday}
+                gender = {cs.gender}
+                job = {cs.job}
+              />
+            )
+          })
+        }
+      </div>
     );
   }
 }
