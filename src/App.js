d010cd365f4cb7cd7971c4f34873dad60f056df8
papav-hub <pavi03@kpu.ac.kr>
2021-01-18 22:36:06 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -2,56 +2,100 @@ import React, { Component } from 'react';
 import Customer from './components/Customer'
 import './App.css';
 
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+
+import {withStyles} from '@material-ui/core/styles';
+// css
+
+const style = thme => ({
+  root : {
+    width : '100%',
+    marginTop : thme.spacing.unit*3,
+    overflowX : "auto"
+    // x축으로 오버플로우 가능
+  },
+  table : {
+    minWidth : 1080
+    // 화면 크기 1080 고정 -> 넘어가면 스크롤바 생김
+  }
+})
+
 const c = [
+    {
+    'id' : 1,
+    'image' : 'http://placeimg.com/100/100/1',
+    'name' : '최혜민',
+    'birthday' : '000323',
+    'gender' : '여자',
+    'job' : '백수이고 싶지만 취직하고 싶은 백수'
+  },
   {
-  'id' : 1,
-  'image' : 'http://placeimg.com/300/300/1',
-  'name' : '최혜민',
-  'birthday' : '000323',
-  'gender' : '여자',
-  'job' : '백수이고 싶지만 취직하고 싶은 백수'
-},
-{
-  'id' : 2,
-  'image' : 'http://placeimg.com/300/300/2',
-  'name' : '최형우',
-  'birthday' : '070607',
-  'gender' : '남자',
-  'job' : '한 손에 흑염룡이 잠든 중2'
-},
-{
-  'id' : 3,
-  'image' : 'http://placeimg.com/300/300/3',
-  'name' : '최준혁',
-  'birthday' : '000305',
-  'gender' : '남자',
-  'job' : '곧 군대가는 멍청이'
-}
+    'id' : 2,
+    'image' : 'http://placeimg.com/100/100/2',
+    'name' : '최형우',
+    'birthday' : '070607',
+    'gender' : '남자',
+    'job' : '한 손에 흑염룡이 잠든 중2'
+  },
+  {
+    'id' : 3,
+    'image' : 'http://placeimg.com/100/100/3',
+    'name' : '최준혁',
+    'birthday' : '000305',
+    'gender' : '남자',
+    'job' : '곧 군대가는 멍청이'
+  }
 ]
 // customer 라는 변수를 만듦
 // 위의 링크는 랜덤으로 image 보여주는 사이트 / 64/64/는 이미지 사이즈
 
 class App extends Component {
   render() {
+  
+  const { classes } = this.props;
+
     return(
-      <div>
-        {
-          c.map(cs => {
-            return (
-              <Customer
-                key = {cs.id}
-                // id값을 key로 사용하겠다. (map 함수 사용할때 사용)
-                id = {cs.id}
-                image = {cs.image}
-                name = {cs.name}
-                birthday = {cs.birthday}
-                gender = {cs.gender}
-                job = {cs.job}
-              />
-            )
-          })
-        }
-      </div>
+      <Paper className = { classes.root }>
+        <Table className = {classes.table }>
+          <TableHead>
+            <TableRow>
+              <TableCell> 번호 </TableCell>
+            <TableCell> 이미지 </TableCell>
+            <TableCell> 이름 </TableCell>
+            <TableCell> 생년월일 </TableCell>
+            <TableCell> 성별 </TableCell>
+            <TableCell> 직업 </TableCell>
+            </TableRow>
+            
+          </TableHead>
+          <TableBody>
+            {
+            c.map(cs => {
+              return (
+                <Customer
+                  key = {cs.id}
+                  // id값을 key로 사용하겠다. (map 함수 사용할때 사용)
+                  id = {cs.id}
+                  image = {cs.image}
+                  name = {cs.name}
+                  birthday = {cs.birthday}
+                  gender = {cs.gender}
+                  job = {cs.job}
+                />
+              )
+              })
+              }
+          </TableBody>
+        </Table>
+        
+
+        
+      </Paper>
     );
   }
 }
@@ -59,4 +103,4 @@ class App extends Component {
 
 // Component -> 웹 문서에서 보여주기 위한 단위
 
-export default App;
+export default withStyles(style)(App);
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -3,43 +3,26 @@ import React from 'react';
 // react component 라이브러리를 사용하기 위해서 import
 // 다른 것에서 이거 사용하기 위해서 export 
 
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+
 class Customer extends React.Component {
     render(){
         return (
-            <div>
-                <CustomerProfile id={this.props.id} image={this.props.image} name={this.props.name} />
-                <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job} />
-            </div>
+            <TableRow>
+                <TableCell> {this.props.id} </TableCell>
+                <TableCell> <img src={this.props.image} alt="profile"/> </TableCell>
+                {/* <img src={this.props.image + '/images/me.png'} alt="profile"/> */}
+                <TableCell> {this.props.name} </TableCell>
+                <TableCell> {this.props.birthday} </TableCell>
+                <TableCell> {this.props.gender} </TableCell>
+                <TableCell> {this.props.job} </TableCell>
+            </TableRow>
         )
     }
 }
 // customer 클래스는 react의 component 형태로 작성 된 클래스
 // App.js로 간다 슝
-
-
-class CustomerProfile extends React.Component{
-    render(){
-        return (
-            <div>
-                <img src = {this.props.image} art = "profile"/>
-                <h2> {this.props.name} ({this.props.id}) </h2>
-            </div>
-        )
-    }
-}
-
-class CustomerInfo extends React.Component {
-    render(){
-        return (
-            <div>
-                <p> {this.props.birthday} </p>
-                <p>{this.props.gender}</p>
-                <p>{this.props.job}</p>
-            </div>
-        )
-    }
-}
-
 
 
 // data 출력할때는 프롭스 이용
